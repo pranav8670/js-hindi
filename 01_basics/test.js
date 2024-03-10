af6dcbd5937400c73750cb950135f0b01c6ce02f
pranav8670 <63099336+pranav8670@users.noreply.github.com>
2024-03-10 17:02:44 +0000

    index added and basics

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,38 @@
-console.log('pranav');
+"use strict";
+
+console.log("pranav");
+
+function fun1() {
+  let x = 5;
+  x = 20;
+
+  if (1 < 10) {
+    x = 300;
+    console.log(x); 
+  }
+  console.log(x);
+}
+
+// fun1();
+
+let i = 10;
+let j = 10;
+let k = 10;
+
+// console.table([i, j, k]);
+
+// Data type
+// string, number, boolean, bigint, null, undefined, symbol, 
+
+let p = 6;
+console.log(typeof(p));
+
+// for converting any data type to number 
+let l = "36a"
+let lConvert = Number(l);
+console.log(typeof(lConvert));
+console.log(lConvert);
+
+
+
+
