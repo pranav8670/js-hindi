080a7e230ad2ce84784dc2796f2ba28eb300fdbf
pranav8670 <63099336+pranav8670@users.noreply.github.com>
2025-03-18 19:25:40 +0000

    pseduo 0.1

---
+++ 03_pseudo/03_palendrome.js
@@ -0,0 +1,27 @@
+// Palendrome
+
+function isPalendrome(str){
+   // phase1 : remove non-alphanumeric char and transform str toLowerCase
+   let cleanStr = '';
+   for(let i=0; i<str.length; i++){
+      let char = str[i].toLowerCase();
+      if((char<='z' && char>='a') || (char<='9' && char>='0')){
+         cleanStr = cleanStr+char;
+      }
+   }
+   console.log(cleanStr);
+   // phase2 : compare left and right index char
+   let left = 0;
+   let right = cleanStr.length-1;
+   while(left<right){
+     if(cleanStr[left] !== cleanStr[right]){
+        return false
+     };
+     left++;
+     right--;
+   }
+   return true;
+}
+
+let str = "pranavvanarp"
+console.log(isPalendrome(str));
