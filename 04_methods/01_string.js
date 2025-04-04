2c8498724dc2fe8d97357f200330e0f2cce9b045
pranav8670 <63099336+pranav8670@users.noreply.github.com>
2025-04-04 15:38:03 +0000

    string method

---
+++ 04_methods/01_string.js
@@ -0,0 +1,23 @@
+// String methods
+
+const str = "Hello World";
+
+console.log(str.replace(/\s/g, ""));                               // HelloWorld -  to remove space 
+console.log(str.replace(/\s/g, "").toLowerCase().toUpperCase());   // HELLOWORLD -  see we can chaining the mmethods also 
+
+console.log(str.length);                                           // 11
+console.log(str.indexOf("W"));                                     // 6
+console.log(str.substring(1,7));                                   // ello W
+console.log(str.slice(1,7));                                       // ello W - same but we can access -ve index too // substr() removed form JS
+
+console.log(str.split(" "));                                       // [ 'Hello', 'World' ]  - it split from given input and 
+                                                                   // put it in a array
+
+console.log(str.includes("W"));                                    // true - same methods we can use in array
+
+
+const str1  = "    Hello     ";
+console.log(str1.trim());                                          // Hello
+
+console.log(str.concat(str1));                                     // to concat string 
+
