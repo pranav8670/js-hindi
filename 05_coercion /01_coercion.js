5c394a755189c2a6a518432ff8fd696e48f75409
pranav8670 <63099336+pranav8670@users.noreply.github.com>
2025-04-04 19:15:28 +0000

    coercion

--- 05_coercion /01_coercion.js
+++ 05_coercion /01_coercion.js
@@ -3,3 +3,9 @@ console.log(null == undefined);                           // true
 console.log(null === undefined);                          // false
 
 console.log([] + []);
+console.log({} + []);
+console.log("5" + 1);
+console.log("5" - 1);
+console.log(false == 0); 
+console.log(false === 0);
+console.log(typeof null);
