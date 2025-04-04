e87cd2d79dc6ebebe90be026d191371308d434ac
pranav8670 <63099336+pranav8670@users.noreply.github.com>
2025-04-04 15:08:18 +0000

    pseudo changes

--- 03_pseudo/07_frequencies.js
+++ 03_pseudo/07_frequencies.js
@@ -20,8 +20,29 @@ function frequencies(arr) {
     return resultArr;
 }
 
-
-
-
-
 console.log(frequencies([1, 1, 1, 5, 8, 8, 8, 9, 9, 0, 0, 6, 5, 4]))
+
+///////////////
+
+function repeat(str) {
+    let myMap = new Map();
+    const newStr = str.replace(/\s/g, "").toLowerCase();
+    console.log(newStr)
+
+    for (let char of newStr) {
+        if (myMap.has(char) && char !== "") {
+            myMap.set(char, myMap.get(char) + 1);
+        } else {
+            myMap.set(char, 1)
+        }
+    }
+
+    let resultMap = []
+    for (let [key, value] of myMap) {
+        resultMap.push(`${key}: ${value}`)
+    }
+    return resultMap
+}
+
+let str = 'This is a string'
+console.log(repeat(str));
--- 03_pseudo/14_moveChar.js
+++ 03_pseudo/14_moveChar.js
@@ -1,11 +1,13 @@
+// 
+// input : angular , need output : ularang
+
 function moveChar(str){
     let iteration  = 0;
     for(let i=0; i<3; i++){
-        console.log(str)
         str = str + str[i];
         str=str.replace(str[i], "")
         iteration = i;
-        console.log(i+1)
+        console.log(i)
     }
     return iteration
 }
