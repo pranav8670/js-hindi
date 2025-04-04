df783378c4b39c0df0b2c8adb77553905d0caaf7
pranav8670 <63099336+pranav8670@users.noreply.github.com>
2025-04-04 18:58:09 +0000

    changes

--- 03_pseudo/14_moveChar.js
+++ 03_pseudo/14_moveChar.js
@@ -6,9 +6,8 @@ function moveChar(str){
     for(let i=0; i<3; i++){
         str = str + str[i];
         str=str.replace(str[i], "")
-        iteration = i;
-        console.log(i)
+        iteration++
     }
     return iteration
 }
-moveChar('angular');
+console.log(moveChar('angular'));
---
+++ 05_coercion /01_coercion.js
@@ -0,0 +1,5 @@
+console.log("5" - true);                                  // 4
+console.log(null == undefined);                           // true
+console.log(null === undefined);                          // false
+
+console.log([] + []);
