e08f77238ab62764ee830fa2b27af34f6c688ce3
pranav8670 <63099336+pranav8670@users.noreply.github.com>
2025-04-22 06:09:56 +0000

    dumplicate

--- 03_pseudo/15_duplicateAndDistinct.js
+++ 03_pseudo/15_duplicateAndDistinct.js
@@ -1,20 +1,20 @@
 // distinct and duplicate values of array
 
 function duplicate(arr){
-    let duplicate = [];
-    let exist = [];
+    let duplicate = new Set();
+    let exist = new Set();
     
     for(let num of arr){
-        if(exist.includes(num)){
-            duplicate.push(num);
+        if(exist.has(num)){
+            duplicate.add(num);
         }else{
-            exist.push(num);
+            exist.add(num);
         }
     }
-    console.log(duplicate);
-    console.log(exist);
+    console.log([...duplicate]);
+    console.log([...exist]);
 }
 
 
-arr = [12,32,3,4324,54,5456,56,23,42,413,13,14,32,3,4324,54];
+arr = [12,32,32,3,4324,54,5456,56,23,42,413,13,14,32,3,4324,54];
 console.log(duplicate(arr));
